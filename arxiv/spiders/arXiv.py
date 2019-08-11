19de0c5a23f9c01c3cf9d570c74df02a2940a51c
youzhiyong <youzhiyong@lxslc602.ihep.ac.cn>
2019-08-11 22:53:00 +0800

    first step complete version---get url from list/astro-ph/list/astro-ph

--- arxiv/spiders/arXiv.py
+++ arxiv/spiders/arXiv.py
@@ -8,9 +8,21 @@ from bs4 import BeautifulSoup
 class ArxivSpider(scrapy.Spider):
     name = 'arXiv'
     allowed_domains = ['arxiv.org']
-    start_urls = ['https://arxiv.org/abs/1904.09130']
+    start_urls = ['https://arxiv.org/list/astro-ph/1904?show=10000']
+    #start_urls = ['https://arxiv.org/abs/1904.09130']
 
     def parse(self, response):
+        regular = re.compile(r'/abs/\d{4}.\d{5}')
+        lis = response.text
+        for href in regular.findall(lis):
+            try:
+                url = 'https://arxiv.org'+href
+                print(url)
+                yield scrapy.Request(url,callback=self.parse_essay)
+            except:
+                continue
+
+    def parse_essay(self, response):
         essay = response.text
         soup = BeautifulSoup(essay,'html.parser')
         item = ArxivItem()
